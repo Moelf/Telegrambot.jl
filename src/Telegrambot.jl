95164105955ff8657f2e8d30f68c9c346baf7065
Simeon Schaub <simeondavidschaub99@gmail.com>
2018-09-20 21:19:45 +0200

    implemented basic InlineQueries some clarification of previous code

--- src/Telegrambot.jl
+++ src/Telegrambot.jl
@@ -3,7 +3,13 @@ module Telegrambot
 import HTTP, Test, JSON
 export startBot, getUpdates, sendText
 
-function startBot(botApi="";textHandle=Dict())
+struct InlineQueryResultArticle
+	id::String
+	title::String
+	message::String
+end
+
+function startBot(botApi=""; textHandle=Dict(), inlineQueryHandle=(s->InlineQueryResultArticle[]))
     if isempty(textHandle)
         error("You need to pass repond function as parameter to startBot")
     end
@@ -12,66 +18,78 @@ function startBot(botApi="";textHandle=Dict())
     while true
         msgQuery = getUpdates(botApi,offset)
         #= JSON.print(msgQuery, 2) =#
-        offsetList = Number[]
-        pendingList = Dict[]
         if length(msgQuery)==0
             continue #this repeast every timeout seconds, now 30
         end
-        for i in 1:length(msgQuery) # update timeout
-            push!(offsetList, msgQuery[i]["update_id"])
-        end
-        offset = maximum(offsetList)+1
+		offset = maximum([ i["update_id"] for i in msgQuery ]) + 1
 
-        try 
-            for i in 1:length(offsetList)
-                push!(pendingList, Dict())
-                pendingList[i][:text] = msgQuery[i]["message"]["text"]
-                pendingList[i][:chat_id] = msgQuery[i]["message"]["chat"]["id"]
-            end
-        catch
-            @warn backtrace()
-            continue
-        end
+        for rawCmd in msgQuery
+            #println(rawCmd) #debug
+			if haskey(rawCmd, "message")
+				msg = rawCmd["message"]
+            	cmdName=" "
+            	cmdPara = " "
+            	try
+            	    cmdName = string(match(r"/([^@\s]+)", msg["text"])[1]) #match till first @ or space
+            	catch
+            	    @warn "Not a command start with /"
+            	    continue
+            	end
 
-        for rawCmd in pendingList
-            println(rawCmd) #deubg
-            cmdName=" "
-            cmdPara = " "
-            try
-                cmdName = string(match(r"/([^@\s]+)", rawCmd[:text])[1]) #match till first @ or space
-            catch
-                @warn "Not a command start with /"
-                continue
-            end
+            	try
+            	    cmdPara = string(match(r"\s(.*)$", msg["text"])[1]) #match from first space to end
+            	catch
+            	    cmdPara = " "
+            	    @warn "Command may got passed empty parameter"
+            	end
+            	#= println("namc: $cmdName, para: $cmdPara") #debug =#
+            	if haskey(textHandle, cmdName)
+            	    reply = textHandle[cmdName](cmdPara) |> HTTP.URIs.escapeuri #encode for GET purpose
+					reply_id = string(msg["chat"]["id"]) |> HTTP.URIs.escapeuri #encode for GET purpose
 
-            try
-                cmdPara = string(match(r"\s(.*)$", rawCmd[:text])[1]) #match from first space to end
-            catch
-                cmdPara = " "
-                @warn "Command may got passed empty parameter"
-            end
-            #= println("namc: $cmdName, para: $cmdPara") #debug =#
-            if haskey(textHandle, cmdName)
-                reply = textHandle[cmdName](cmdPara)
-                reply = HTTP.URIs.escapeuri(reply) #encode for GET purpose
-                reply_id = string(rawCmd[:chat_id])
-                reply_id = HTTP.URIs.escapeuri(reply_id) #encode for GET purpose
+            	    sendText(botApi, reply_id, reply)
+            	else
+            	    @warn backtrace()
+            	end
+			end
+			if haskey(rawCmd, "inline_query")
+				qry = rawCmd["inline_query"]
+				#println(qry["query"])
 
-                sendText(botApi, reply_id, reply)
-            else
-                @warn backtrace()
-            end
+				if qry["query"]≠""
+					results = inlineQueryHandle(qry["query"]) |> toJSON |> HTTP.URIs.escapeuri #encode for GET purpose
+					query_id = qry["id"] |> HTTP.URIs.escapeuri #encode for GET purpose
+
+					answerInlineQuery(botApi, query_id, results)
+				end
+
+			end
         end
     end
 end
 
-function sendText(botApi, chat_id,text)
+function sendText(botApi, chat_id, text)
     tQuery="""chat_id=$chat_id&text=$text"""
     updates = HTTP.request("GET","https://api.telegram.org/$botApi/sendMessage";query="$tQuery")
     sleep(0.1)
 end
 
-function getUpdates(botApi="",offset=0)
+function answerInlineQuery(botApi, query_id, results::String)
+	tQuery="""inline_query_id=$query_id&results=$results"""
+    updates = HTTP.request("GET","https://api.telegram.org/$botApi/answerInlineQuery";query="$tQuery")
+    sleep(0.1)
+end
+
+function toJSON(articles::Array{InlineQueryResultArticle})
+	s = JSON.json([ Dict("type"=>"article","id"=>article.id, "title"=>article.title,
+							"input_message_content"=>
+							Dict{String,String}("message_text"=>article.message ) )
+					  for article in articles])
+	#print(s)
+	return s
+end
+
+function getUpdates(botApi="", offset=0)
     tQuery="""timeout=30&offset=$offset"""
     updates = JSON.parse(String(HTTP.request("GET","https://api.telegram.org/$botApi/getUpdates";query="$tQuery").body))
     #= JSON.print(updates, 2) =#
